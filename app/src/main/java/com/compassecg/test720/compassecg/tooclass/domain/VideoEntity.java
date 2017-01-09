36343170e3d0ea81d96d32e2b48784dbc8c5c483
hunan <497370715@qq.com>
2017-01-09 16:48:19 +0800

    recover HW

--- app/src/main/java/com/compassecg/test720/compassecg/tooclass/domain/VideoEntity.java
+++
@@ -1,9 +0,0 @@
-package com.compassecg.test720.compassecg.tooclass.domain;
-
-public class VideoEntity {
-	public int ID;
-	public String title;
-	public String filePath;
-	public int size;
-	public int duration;
-}
